c3856628128399323ecc09ed2f23499e11ec7013
andruraj <andrewraj10@gmail.com>
2019-07-30 23:54:43 +0530

    heroku push staticfiles

--- devforum/settings.py
+++ devforum/settings.py
@@ -125,6 +125,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
